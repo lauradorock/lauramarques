9f9629d1ed34e1ba8d6c674af6bf12bff281dd02
laura <84184642+lauradorock@users.noreply.github.com>
2023-03-14 12:18:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("ola mundo")
+const nome="laura"
+console.log(nome)
+
+let idade = 17
+console.log(idade)
+
+const idadeAluno = 18
+
+const nomeAluno = "Olivia"
+
+const triste = false
+
+console.log(idadeAluno, nomeAluno, triste)
+
